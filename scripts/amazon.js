0ad097b083c5fa21fb6ee92d5cbf6431b4003627
Kyle <klim385@aucklanduni.ac.nz>
2024-07-04 04:13:27 +1200

    Added message appears when 'Add to Cart' button is pressed

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -40,7 +40,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart" id="js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -58,14 +58,32 @@ const cartQuantityElement = document.getElementById('js-cart-quantity');
 document.getElementById('js-products-grid')
   .innerHTML = productsHTML;
 
+const addedMessageTimeouts = {};  
+
 document.querySelectorAll('#js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const { productId } = button.dataset;
       const quantity = Number(document.
         querySelector(`#js-quantity-selector-${productId}`).value);
+      
+      const addedMessage = document.querySelector(
+        `#js-added-to-cart-${productId}`
+      );
+      
+      addedMessage.classList.add('added-to-cart-visible');
 
-      console.log(quantity);
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+
+      addedMessageTimeouts[productId] = timeoutId;
+
       let matchingItem;
 
       cart.forEach((item) => {
