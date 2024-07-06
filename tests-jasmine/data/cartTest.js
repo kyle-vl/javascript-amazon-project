98ac8d24c388063ccb0214d2746086878913019c
Kyle <klim385@aucklanduni.ac.nz>
2024-07-06 22:52:10 +1200

    Create automated tests for add to cart

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,43 @@
+import {addToCart, cart, loadFromStorage} from "../../data/cart.js";
+
+describe('test suite: addToCart', () => {
+  it('adds an existing product to the cart', () => {
+    /* Mock setItem so the test case does 
+    not save the test cart variable */
+    spyOn(localStorage, 'setItem');
+
+    /* Mock getItem and return a cart with a test product */
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 1);
+
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(2);
+  })
+
+  it('adds a new product to the cart', () => {
+    /* Mock setItem so the test case does 
+    not save the test cart variable */
+    spyOn(localStorage, 'setItem');
+
+    /* Mock getItem and return an empty cart to ensure
+    the test cart does not have the default values */
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]);
+    });
+    loadFromStorage();
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 1);
+
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+  })
+})
