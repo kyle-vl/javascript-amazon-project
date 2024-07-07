4b2deeb5678ec543de45fb30d47a5932747ff2a5
Kyle <klim385@aucklanduni.ac.nz>
2024-07-08 02:25:05 +1200

    Improved search results by ignoring letter case and searching keywords

--- data/products.js
+++ data/products.js
@@ -19,6 +19,7 @@ class Product {
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
+    this.keywords = productDetails.keywords;
   }
 
   getStarsUrl() {
